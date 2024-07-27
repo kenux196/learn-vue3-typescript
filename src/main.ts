b65c337bdf38815df1631d2868740a62c29a15b1
kenux196 <kenux.yun@gmail.com>
2024-07-27 10:07:29 +0900

    setup plugins

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,35 @@
-import './assets/main.css'
+import './assets/main.css';
 
-import { createApp } from 'vue'
-import { createPinia } from 'pinia'
+import { createApp } from 'vue';
+import { createPinia } from 'pinia';
+import { Quasar, SessionStorage, Notify } from 'quasar';
+import { VueQueryPlugin } from 'vue-query';
 
-import App from './App.vue'
-import router from './router'
+import App from './App.vue';
+import router from './router';
 
-const app = createApp(App)
+import piniaPluginPersistedState from 'pinia-plugin-persistedstate';
 
-app.use(createPinia())
-app.use(router)
+// import icon libs
+import '@quasar/extras/material-icons/material-icons.css';
+// import quasar css
+// import 'quasar/dist/quasar.css';
+import 'quasar/src/css/index.sass';
 
-app.mount('#app')
+const pinia = createPinia();
+pinia.use(piniaPluginPersistedState);
+
+const app = createApp(App);
+
+app.config.errorHandler = (err: any) => {
+  console.error(err);
+};
+
+app.use(pinia);
+app.use(router);
+app.use(Quasar, {
+  plugins: { SessionStorage, Notify },
+});
+app.use(VueQueryPlugin);
+
+app.mount('#app');
