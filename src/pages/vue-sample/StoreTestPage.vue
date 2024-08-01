76042a231fae16bde6d3d6d40fceda8714cae1fe
kenux196 <kenux.yun@gmail.com>
2024-08-01 23:02:08 +0900

    modify pinia-plugin-persistedstate 설정

--- src/pages/vue-sample/StoreTestPage.vue
+++ src/pages/vue-sample/StoreTestPage.vue
@@ -8,6 +8,15 @@
     <button @click="counterStore.$reset">초기화</button>
     <p v-if="isOver10">{{ count }}가 10보다 커졌습니다.</p>
   </div>
+  <div class="grid">
+    <p>optionStore.state.name: {{ optionStore.name }}</p>
+    <input type="text" v-model="optionStore.name" />
+    <p>{{ optionStore.count }} * 2 = {{ optionStore.doubleCount }}</p>
+    <button @click="optionStore.increment">count 증가</button>
+    <button @click="optionStore.decrement">count 감소</button>
+    <button @click="optionStore.$reset">초기화</button>
+    <p v-if="isOver10">{{ count }}가 10보다 커졌습니다.</p>
+  </div>
   <div>
     <p>
       accessToken: {{ accessToken }} <br />
@@ -25,10 +34,11 @@
 <script setup lang="ts">
 import { ref } from 'vue';
 import { storeToRefs } from 'pinia';
-import { useCounterStore, useUserStore } from '../../stores/store';
+import { useCounterStore, useUserStore, useCounterOptionApiStore } from '../../stores/store';
 
 const counterStore = useCounterStore();
 const userStore = useUserStore();
+const optionStore = useCounterOptionApiStore();
 
 // 반응형을 유지하면서 스토어에서 속성 추출하기: storeToRefs() 사용
 // 스토어의 상태만 사용하고, 액션을 호출하지 않을 때 유용한 방법이다.
--- src/stores/store.ts
+++ src/stores/store.ts
@@ -11,27 +11,29 @@ export const store = reactive({
 });
 
 // setup store 방식
-export const useCounterStore = defineStore('counter', () => {
-  const count = ref(0);
-  const name = ref('Eduardo');
-  const doubleCount = computed(() => count.value * 2);
+export const useCounterStore = defineStore(
+  'counter',
+  () => {
+    const count = ref(0);
+    const name = ref('Eduardo');
+    const doubleCount = computed(() => count.value * 2);
 
-  function increment() {
-    count.value++;
-  }
+    function increment() {
+      count.value++;
+    }
 
-  function decrement() {
-    count.value--;
-  }
+    function decrement() {
+      count.value--;
+    }
 
-  function $reset() {
-    count.value = 0;
-  }
-
-  const persist = () => {
-    return {
-      enable: true,
-      strategies: [{ storage: localStorage }],
+    function $reset() {
+      count.value = 0;
+    }
+    return { count, name, doubleCount, increment, decrement, $reset };
+  },
+  {
+    persist: {
+      storage: localStorage,
       beforeRestore: (ctx: PiniaPluginContext) => {
         // 복구전 호출되는 콜백
         console.log(`about to restore '${ctx.store.$id}`);
@@ -40,11 +42,9 @@ export const useCounterStore = defineStore('counter', () => {
         // 복구 후 호출되는 콜백
         console.log(`just restored '${ctx.store.$id}'`);
       },
-    };
-  };
-
-  return { count, name, doubleCount, increment, decrement, $reset, persist };
-});
+    },
+  }
+);
 
 export const useCounterOptionApiStore = defineStore('counter2', {
   state: () => {
@@ -66,31 +66,44 @@ export const useCounterOptionApiStore = defineStore('counter2', {
       this.count--;
     },
   },
+  persist: {
+    storage: localStorage, // default : localstorage
+    beforeRestore: (ctx: PiniaPluginContext) => {
+      // 복구전 호출되는 콜백
+      console.log(`about to restore '${ctx.store.$id}`);
+    },
+    afterRestore: (ctx: PiniaPluginContext) => {
+      // 복구 후 호출되는 콜백
+      console.log(`just restored '${ctx.store.$id}'`);
+    },
+  },
 });
 
-export const useUserStore = defineStore('storeUser', () => {
-  const firstName = ref('firstName');
-  const lastName = ref('lastName');
-  const accessToken = ref('xxxxxxxxxxxxxxxxxxxxxxx');
+export const useUserStore = defineStore(
+  'storeUser',
+  () => {
+    const firstName = ref('firstName');
+    const lastName = ref('lastName');
+    const accessToken = ref('xxxxxxxxxxxxxxxxxxxxxxx');
 
-  function setName(fName: string, lName: string) {
-    firstName.value = fName;
-    lastName.value = lName;
-  }
+    function setName(fName: string, lName: string) {
+      firstName.value = fName;
+      lastName.value = lName;
+    }
 
-  function setToken(value: string) {
-    accessToken.value = value;
-  }
+    function setToken(value: string) {
+      accessToken.value = value;
+    }
 
-  function $reset() {
-    firstName.value = '';
-    lastName.value = '';
-    accessToken.value = '';
-  }
-
-  const persist = () => {
-    return {
-      enabled: true,
+    function $reset() {
+      firstName.value = '';
+      lastName.value = '';
+      accessToken.value = '';
+    }
+    return { firstName, lastName, accessToken, setName, setToken, $reset };
+  },
+  {
+    persist: {
       storage: localStorage, // default : localstorage
       paths: ['accessToken', 'lastName'], // 특정값만 지정해서 저장.
       beforeRestore: (ctx: PiniaPluginContext) => {
@@ -101,7 +114,6 @@ export const useUserStore = defineStore('storeUser', () => {
         // 복구 후 호출되는 콜백
         console.log(`just restored '${ctx.store.$id}'`);
       },
-    };
-  };
-  return { firstName, lastName, accessToken, setName, setToken, $reset, persist };
-});
+    },
+  }
+);
