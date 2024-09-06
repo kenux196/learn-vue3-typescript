96cf70a59b3ebaf13c4c8d929cf71d1bab631b2f
kenux196 <kenux.yun@gmail.com>
2024-09-07 01:00:12 +0900

    some fix

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,15 +1,21 @@
 /* eslint-env node */
-require('@rushstack/eslint-patch/modern-module-resolution')
+require('@rushstack/eslint-patch/modern-module-resolution');
 
 module.exports = {
   root: true,
-  'extends': [
+  extends: [
     'plugin:vue/vue3-essential',
     'eslint:recommended',
     '@vue/eslint-config-typescript',
-    '@vue/eslint-config-prettier/skip-formatting'
+    '@vue/eslint-config-prettier/skip-formatting',
   ],
+  rules: {
+    'no-var': 'error',
+    quotes: ['error', 'single'],
+    semi: ['error', 'always'],
+    'no-unused-vars': 'off',
+  },
   parserOptions: {
-    ecmaVersion: 'latest'
-  }
-}
+    ecmaVersion: 'latest',
+  },
+};
--- src/router/index.ts
+++ src/router/index.ts
@@ -1,19 +1,19 @@
 import {
   createRouter,
-  createMemoryHistory,
+  // createMemoryHistory,
   createWebHistory,
-  createWebHashHistory,
+  // createWebHashHistory,
 } from 'vue-router';
 import routes from './routes';
 
-const createHistory = import.meta.env.VITE_SERVER
-  ? createMemoryHistory
-  : import.meta.env.VITE_VUE_ROUTER_MODE === 'history'
-    ? createWebHistory
-    : createWebHashHistory;
+// const createHistory = import.meta.env.VITE_SERVER
+//   ? createMemoryHistory
+//   : import.meta.env.VITE_VUE_ROUTER_MODE === 'history'
+//     ? createWebHistory
+//     : createWebHashHistory;
 
 const router = createRouter({
-  history: createHistory(import.meta.env.BASE_URL),
+  history: createWebHistory(import.meta.env.BASE_URL),
   routes,
   scrollBehavior: () => ({ left: 0, top: 0 }),
 });
