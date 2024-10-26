05cc2b3d93b24aa44b4a9d00a315ff5c47861ad9
kenux196 <kenux.yun@gmail.com>
2024-10-26 16:57:30 +0900

    Modify router error for echart page

--- src/pages/chart-sample/ChartMainPage.vue
+++ src/pages/chart-sample/ChartMainPage.vue
@@ -2,9 +2,7 @@
   <div class="q-pa-md">
     <div class="q-guttoer-y-md">
       <q-tabs v-model="tab" inline-label align="left" class="bg-primary text-white shadow-2">
-        <q-route-tab name="chart1" icon="analytics" label="chart.js-1" to="/chart-js/chart1" />
-        <q-route-tab name="chart2" icon="trending_up" label="chart.js-2" to="/chart-js/chart2" />
-        <q-route-tab name="echart" icon="trending_up" label="echart" to="/chart-js/echart" />
+        <q-route-tab name="echart" icon="trending_up" label="echart" to="/chart/echart" />
       </q-tabs>
     </div>
     <RouterView class="q-px-md" />
@@ -16,5 +14,3 @@ import { ref } from 'vue';
 
 const tab = ref('chart1');
 </script>
-
-<style lang="scss" scoped></style>
