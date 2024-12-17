2232dee45801272dbdc58a4526564befeb40aa86
skyun <skyun@a2tec.co.kr>
2024-12-17 15:24:12 +0900

    Add unit test sample

--- src/components/__tests__/HelloWorld.spec.ts
+++ src/components/__tests__/HelloWorld.spec.ts
@@ -1,11 +1,20 @@
-import { describe, it, expect } from 'vitest'
+import { describe, it, expect, test } from 'vitest';
 
-import { mount } from '@vue/test-utils'
-import HelloWorld from '../HelloWorld.vue'
+import { mount } from '@vue/test-utils';
+import HelloWorld from '../HelloWorld.vue';
 
 describe('HelloWorld', () => {
   it('renders properly', () => {
-    const wrapper = mount(HelloWorld, { props: { msg: 'Hello Vitest' } })
-    expect(wrapper.text()).toContain('Hello Vitest')
-  })
-})
+    const wrapper = mount(HelloWorld, { props: { msg: 'Hello Vitest' } });
+    expect(wrapper.text()).toContain('Hello Vitest');
+  });
+});
+
+describe('describe는 여러 테스트를 그룹으로 묶는 역할', () => {
+  test('테스트 내용을 적는다.', () => {
+    expect('test'.length).toBe(4);
+  });
+  it('split을 이용한 문자열 분리', () => {
+    expect('hello unit test'.split(' ').length).toBe(3);
+  });
+});
