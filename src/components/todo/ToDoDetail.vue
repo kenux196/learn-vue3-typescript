e23180f4d3cf23736618d821e6dbab3acdbeba02
skyun <skyun@a2tec.co.kr>
2024-12-19 13:18:21 +0900

    modify with copilot

--- src/components/todo/ToDoDetail.vue
+++ src/components/todo/ToDoDetail.vue
@@ -1,7 +1,8 @@
 <template>
   <p>할 일 id: {{ todoId }}</p>
-  <button @click="todoId++">다음 할 일 가져오기</button>
-  <p v-if="!todoData">로딩...</p>
+  <button @click="incrementTodoId">다음 할 일 가져오기</button>
+  <p v-if="!todoData && !error">로딩...</p>
+  <p v-if="error">에러 발생: {{ error }}</p>
   <pre v-else>{{ todoData }}</pre>
 </template>
 
@@ -11,14 +12,22 @@ import { onMounted, ref, watch } from 'vue';
 
 const todoId = ref(1);
 const todoData = ref(null);
+const error = ref<string | null>(null);
 
 async function fetchData() {
+  todoData.value = null;
+  error.value = null;
   try {
     const res = await JsonplaceHolderApiService.fetchTodoById(todoId.value);
     todoData.value = res.data;
   } catch (e) {
     console.error(e);
+    error.value = '데이터를 불러오는 중 에러가 발생했습니다.';
   }
+}
+
+function incrementTodoId() {
+  todoId.value++;
 }
 
 onMounted(() => {
