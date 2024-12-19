a9349ac0bf81b31b5efeae5d0d9aebd6c5bac3ba
skyun <skyun@a2tec.co.kr>
2024-12-19 13:53:24 +0900

    modify with copilot

--- src/components/book/BookAddForm.vue
+++ src/components/book/BookAddForm.vue
@@ -7,7 +7,7 @@
       <label for="author">작가</label>
       <input id="author" type="text" placeholder="작가명 입력" v-model="state.author" />
       <label for="price">가격</label>
-      <input id="price" type="number" placeholder="제목을 입력하세요" v-model="state.price" />
+      <input id="price" type="number" placeholder="가격을 입력하세요" v-model="state.price" />
       <label for="published-date">발행일</label>
       <input id="published-date" type="date" v-model="state.date" />
       <div class="grid">
@@ -47,22 +47,21 @@ function close() {
 
 function validateInputValue() {
   if (state.title == '' || state.author == '' || state.price == 0 || state.date == '') {
-    state;
     return false;
   }
   return true;
 }
 
 function addBook() {
-  if (validateInputValue()) {
-    const book = Book.createBook(state.title, state.author, state.price, state.date);
-    bookMemoryRepository.save(book);
-    console.log('book 추가하기 성공');
-    emit('addedBook');
-    close();
-  } else {
-    alert('책 정보를 입력하세요~~~');
+  if (!validateInputValue()) {
+    alert('모든 필드를 채워주세요.');
+    return;
   }
+
+  const newBook = new Book(state.title, state.author, parseFloat(state.price), state.date);
+  bookMemoryRepository.add(newBook);
+  emit('addedBook', newBook);
+  close();
 }
 
 // defineExpose를 통하여 함수를 내보낸다.
