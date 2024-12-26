5f04b189505b2fce7c7c0417e1084d22063c71e4
skyun <skyun@a2tec.co.kr>
2024-12-26 16:45:37 +0900

    Modify vue test sample page

--- src/pages/vue-sample/VueSampleMainPage.vue
+++ src/pages/vue-sample/VueSampleMainPage.vue
@@ -1,23 +1,31 @@
 <template>
-  <div class="q-pa-md">
-    <div class="q-guttoer-y-md">
-      <q-tabs v-model="tab" inline-label align="left" class="bg-primary text-white shadow-2">
-        <q-route-tab name="emit" label="Emit" to="/vue/emit" />
-        <q-route-tab name="todo-list1" label="todo-list1" to="/vue/todo-list1" />
-        <q-route-tab name="todo-list2" label="todo-list2" to="/vue/todo-list2" />
-        <q-route-tab name="store" label="Store" to="/vue/store" />
-        <q-route-tab name="utils" label="utils" to="/vue/utils" />
-        <q-route-tab name="login" label="login" to="/vue/login" />
-        <q-route-tab name="vue-query" label="vue-query" to="/vue/vue-query" />
-        <q-route-tab name="language" label="language" to="/vue/i18n" />
-      </q-tabs>
-    </div>
-    <RouterView />
+  <div role="tablist" class="tabs tabs-bordered mb-10">
+    <a role="tab" class="tab" href="/vue/todo-list1">Todo List 1</a>
+    <a role="tab" class="tab" href="/vue/todo-list2">Todo List 2</a>
+    <a role="tab" class="tab" href="/vue/store">Store</a>
+    <a role="tab" class="tab" href="/vue/utils">Utils</a>
+    <a role="tab" class="tab" href="/vue/login">Login</a>
+    <a role="tab" class="tab" href="/vue/vue-query">Vue Query</a>
+    <a role="tab" class="tab" href="/vue/i18n">Language</a>
   </div>
+  <!-- <div v-for="subMenu in subMenus" :key="subMenu.id" role="tablist" class="tabs tabs-bordered">
+    <a role="tab" class="tab" :href="subMenu.path">{{ subMenu.name }}</a>
+  </div> -->
+  <RouterView />
 </template>
 
 <script setup lang="ts">
 import { ref } from 'vue';
 
-const tab = ref('utils');
+const subMenus = ref([
+  { id: 1, name: 'Emit', path: '/vue/emit' },
+  { id: 2, name: 'Todo List 1', path: '/vue/todo-list1' },
+  { id: 3, name: 'Todo List 2', path: '/vue/todo-list2' },
+  { id: 4, name: 'Store', path: '/vue/store' },
+  { id: 5, name: 'Utils', path: '/vue/utils' },
+  { id: 6, name: 'Login', path: '/vue/login' },
+  { id: 7, name: 'Vue Query', path: '/vue/vue-query' },
+  { id: 8, name: 'Language', path: '/vue/i18n' },
+  { id: 9, name: 'Tab 2', path: '/vue/tab2' },
+]);
 </script>
