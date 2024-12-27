f204659e70e2877f7a6b4e3fcffba7af3ea08892
skyun <skyun@a2tec.co.kr>
2024-12-27 11:06:24 +0900

    Modify chart page: remove route

--- src/pages/chart-sample/ChartMainPage.vue
+++ src/pages/chart-sample/ChartMainPage.vue
@@ -1,12 +1,7 @@
 <template>
-  <div role="tablist" class="tabs tabs-bordered mb-10">
-    <a role="tab" class="tab" href="/chart/echart">echart</a>
-  </div>
-  <RouterView />
+  <EchartPie />
 </template>
 
 <script setup lang="ts">
-import { ref } from 'vue';
-
-const tab = ref('chart1');
+import EchartPie from './EchartPieSample.vue';
 </script>
