08fd317badee57f9005bfc267efc05145e075a79
skyun <skyun@a2tec.co.kr>
2024-12-27 10:16:39 +0900

    modify chart page

--- src/pages/chart-sample/ChartMainPage.vue
+++ src/pages/chart-sample/ChartMainPage.vue
@@ -1,12 +1,8 @@
 <template>
-  <div class="q-pa-md">
-    <div class="q-guttoer-y-md">
-      <q-tabs v-model="tab" inline-label align="left" class="bg-primary text-white shadow-2">
-        <q-route-tab name="echart" icon="trending_up" label="echart" to="/chart/echart" />
-      </q-tabs>
-    </div>
-    <RouterView class="q-px-md" />
+  <div role="tablist" class="tabs tabs-bordered mb-10">
+    <a role="tab" class="tab" href="/chart/echart">echart</a>
   </div>
+  <RouterView />
 </template>
 
 <script setup lang="ts">
